5881cee796163a6b2f60b24528e9cd16048ae03e
DungKHoang <DungKHoang@users.noreply.github.com>
2019-08-29 08:18:55 -0700

    Update rename-enclosure.ps1

--- rename-enclosure.ps1
+++ rename-enclosure.ps1
@@ -5,13 +5,13 @@ connect-HPOVMgmt -hostname '<OV-IP-host>' -credential (get-credential)
 $name           = '0000A66101'
 $new_name       = 'BLVC0001'
 
-$this_enclosure = get-HPOVEnclosure -name $this_name
+$this_enclosure = get-HPOVEnclosure -name $name
 if $this_enclosure
 {
-    $uri            = $this_enclosure.uri
+    $uri        = $this_enclosure.uri
 
-    $body = @{"op" = "replace"; "path" = "/name"; "value" = "new_name"}
-    $headers = @{'content-type'= 'application/json'; "If-Match" = '*'}
+    $body       = @{"op" = "replace"; "path" = "/name"; "value" = "new_name"}
+    $headers    = @{'content-type'= 'application/json'; "If-Match" = '*'}
 
 
     send-HPOVRequest -uri $uri "PATCH" $body -AddHeader $headers
